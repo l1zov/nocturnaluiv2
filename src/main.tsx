881383b28a86c50c23eab80f02bdbccda76bfabe
sillyconvicted <130371349+sillyconvicted@users.noreply.github.com>
2025-07-30 13:09:27 +0300

    chore: added packages related to ace

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
-import { loader } from '@monaco-editor/react';
-
-loader.init().then(() => {
-  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  );
-});
+);
