55e8a45b201d07516de8dcdf30d80c1e9f40cebb
l1zov <130371349+l1zov@users.noreply.github.com>
2026-01-15 22:42:54 +0200

    feat: add better macOS traffic light icons

---
+++ src/types/macos-traffic-lights.d.ts
@@ -0,0 +1,12 @@
+declare module 'macos-traffic-lights' {
+  export const unfocused: string;
+  export const closeDefault: string;
+  export const closeHover: string;
+  export const closeActive: string;
+  export const minimizeDefault: string;
+  export const minimizeHover: string;
+  export const minimizeActive: string;
+  export const maximizeDefault: string;
+  export const maximizeHover: string;
+  export const maximizeActive: string;
+}
